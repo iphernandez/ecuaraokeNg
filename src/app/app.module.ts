e5dba2e5dddbc9e7e0bf87ae0f228269c99b2cc4
Hernandez, Ivan P <ivan.p.hernandez@intel.com>
2019-03-08 18:16:41 -0800

    Added header footer and home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './templates/navbar/navbar.component';
+import { FooterComponent } from './templates/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
