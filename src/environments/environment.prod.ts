1c65254444c347c2237b87370277b58fe72fad15
Hernandez, Ivan P <ivan.p.hernandez@intel.com>
2019-03-26 21:48:33 -0700

    Updated api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   appTitle: 'ECUAraoke',
   appDescription: 'Esta es una simple applicación que mantiene una lista de canciones para cantar en un karaoke',
-  apiUrl: 'http://40.91.121.240/api/'
+  apiUrl: 'https://ecuaraoke-api.herokuapp.com/api'
 };
